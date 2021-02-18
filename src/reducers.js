0079c0df5eb61bc43aca812bf5fb7f698dd24fa5
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-02-18 16:23:46 -0600

    increase and decrease functionality

--- src/reducers.js
+++ src/reducers.js
@@ -5,7 +5,30 @@ function reducer(state, action) {
         return { ...state, cart: [] };
     }
     if ( action.type === REMOVE ) {
-        return { ...state}
+        return { ...state, cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id )}
+    }
+    if ( action.type === INCREASE ) {
+        let tempCart = state.cart.map((cartItem) => {
+            if ( action.payload.id === cartItem.id) {
+                return {...cartItem, amount: cartItem.amount + 1 }
+            }
+            return cartItem
+        })
+        return { ...state, cart: tempCart}
+    }
+    if (action.type === DECREASE ) {
+        let tempCart = [];
+        if (action.payload.amount === 1) {
+            tempCart = state.cart.filter((cartItem) => cartItem.id !== action.payload.id )
+        } else {
+            tempCart = state.cart.map(cartItem => {
+                if ( cartItem.id === action.payload.id) {
+                    return {...cartItem, amount: cartItem.amount - 1}
+                }
+                return cartItem;
+            })
+        }
+        return {...state, cart: tempCart};
     }
     return state;
   }
